69a59f34be01644e4407fcd0e4e6585e4c2309cb
Radek Zika <13420016+zikarad@users.noreply.github.com>
2018-09-29 13:35:44 +0200

    security group fixed output text changed

--- networks.tf
+++ networks.tf
@@ -1,82 +1,102 @@
+/* --- VPCs */
 resource "aws_vpc" "vpc-main" {
-	cidr_block	= "10.0.0.0/16"	
+  cidr_block = "10.0.0.0/16"
 
-	tags {
-		Name  = "main"
-		stage = "poc"
-	}
+  tags {
+    Name  = "main"
+    stage = "poc"
+  }
+}
+
+/* ENDPOINTS */
+resource "aws_vpc_endpoint" "endp-s3" {
+  vpc_id       = "${aws_vpc.vpc-main.id}"
+  service_name = "com.amazonaws.${var.region}.s3"
+}
+
+resource "aws_vpc_endpoint" "endp-dydb" {
+  vpc_id       = "${aws_vpc.vpc-main.id}"
+  service_name = "com.amazonaws.${var.region}.dynamodb"
 }
 
 /* NETWORKS */
-
 resource "aws_subnet" "sn-pub1" {
-	vpc_id		= "${aws_vpc.vpc-main.id}"
+  vpc_id = "${aws_vpc.vpc-main.id}"
 
-	cidr_block = "10.0.1.0/24"
-	availability_zone = "eu-central-1a"
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "eu-central-1a"
 
-	tags{
-		Name = "public1"
-		stage = "poc"
-	}
+  tags {
+    Name  = "public1"
+    stage = "poc"
+  }
 }
 
 resource "aws_subnet" "sn-pub2" {
-	vpc_id		= "${aws_vpc.vpc-main.id}"
+  vpc_id = "${aws_vpc.vpc-main.id}"
 
-	cidr_block = "10.0.2.0/24"
-	availability_zone = "eu-central-1b"
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "eu-central-1b"
 
-	tags{
-		Name = "public2"
-		stage = "poc"
-	}
+  tags {
+    Name  = "public2"
+    stage = "poc"
+  }
 }
 
 resource "aws_subnet" "sn-priv1" {
-	vpc_id		= "${aws_vpc.vpc-main.id}"
+  vpc_id = "${aws_vpc.vpc-main.id}"
 
-	cidr_block = "10.0.129.0/24"
-	availability_zone = "eu-central-1a"
+  cidr_block        = "10.0.129.0/24"
+  availability_zone = "eu-central-1a"
 
-	tags{
-		Name = "private1"
-		stage = "poc"
-	}
+  tags {
+    Name  = "private1"
+    stage = "poc"
+  }
 }
 
 resource "aws_subnet" "sn-priv2" {
-	vpc_id		= "${aws_vpc.vpc-main.id}"
+  vpc_id = "${aws_vpc.vpc-main.id}"
 
-	cidr_block = "10.0.130.0/24"
-	availability_zone = "eu-central-1b"
+  cidr_block        = "10.0.130.0/24"
+  availability_zone = "eu-central-1b"
 
-	tags{
-		Name = "private2"
-		stage = "poc"
-	}
+  tags {
+    Name  = "private2"
+    stage = "poc"
+  }
 }
 
 /* GATEWAYs */
-
 resource "aws_internet_gateway" "igw-main" {
   vpc_id = "${aws_vpc.vpc-main.id}"
-	tags {
-		Name = "igw-main"
-	}
+
+  tags {
+    Name = "igw-main"
+  }
+}
+
+resource "aws_vpn_gateway" "vpngw-main" {
+  vpc_id = "${aws_vpc.vpc-main.id}"
+
+  tags {
+    Name = "main"
+  }
 }
 
 resource "aws_eip" "eip-ngw1" {
   vpc = true
 
-  depends_on                = ["aws_internet_gateway.igw-main"]
+  depends_on = ["aws_internet_gateway.igw-main"]
 }
 
 resource "aws_eip" "eip-ngw2" {
   vpc = true
 
-  depends_on                = ["aws_internet_gateway.igw-main"]
+  depends_on = ["aws_internet_gateway.igw-main"]
 }
+
 resource "aws_nat_gateway" "ngw-priv1" {
   allocation_id = "${aws_eip.eip-ngw1.id}"
   subnet_id     = "${aws_subnet.sn-priv1.id}"
@@ -96,22 +116,40 @@ resource "aws_nat_gateway" "ngw-priv2" {
 }
 
 /* ROUTE TABLEs */
+resource "aws_route_table" "rt-pub" {
+  vpc_id = "${aws_vpc.vpc-main.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.igw-main.id}"
+  }
+}
 
 resource "aws_route_table" "rt-priv1" {
-	vpc_id = "${aws_vpc.vpc-main.id}"
+  vpc_id = "${aws_vpc.vpc-main.id}"
 
-	route {
-    cidr_block			= "10.0.129.0/24"
-    nat_gateway_id	= "${aws_nat_gateway.ngw-priv1.id}"
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = "${aws_nat_gateway.ngw-priv1.id}"
   }
-
 }
 
 resource "aws_route_table" "rt-priv2" {
-	vpc_id = "${aws_vpc.vpc-main.id}"
+  vpc_id = "${aws_vpc.vpc-main.id}"
 
-	route {
-		cidr_block			= "10.0.130.0/24"
-		nat_gateway_id	= "${aws_nat_gateway.ngw-priv2.id}"
-	}
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = "${aws_nat_gateway.ngw-priv2.id}"
+  }
+}
+
+/* ROUTE TABLE ASSOCIATION */
+resource "aws_route_table_association" "rta-pub1" {
+  subnet_id      = "${aws_subnet.sn-pub1.id}"
+  route_table_id = "${aws_route_table.rt-pub.id}"
+}
+
+resource "aws_route_table_association" "rta-pub2" {
+  subnet_id      = "${aws_subnet.sn-pub2.id}"
+  route_table_id = "${aws_route_table.rt-pub.id}"
 }
--- vms.tf
+++ vms.tf
@@ -7,30 +7,69 @@ resource "aws_key_pair" "sshkey-gen" {
 	public_key = "${file("${var.sshkey_path}")}"
 }
 
+resource "aws_security_group" "sg-jumphost" {
+  vpc_id = "${aws_vpc.vpc-main.id}"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags {
+    Name = "sg-jumphost"
+  }
+}
+
+
 resource "aws_instance" "vm-jh1" {
 	ami						= "${var.ami}"
 	instance_type = "${var.jh-size}"
 
+	subnet_id			= "${aws_subnet.sn-pub1.id}"
+	security_groups = ["${aws_security_group.sg-jumphost.id}"]
 	key_name = "${var.sshkey_name}"
 	associate_public_ip_address = true
 
-} 
+	tags {
+		Name = "jh1"
+	}
+}
 
 resource "aws_instance" "vm-jh2" {
 	ami						= "${var.ami}"
 	instance_type = "${var.jh-size}"
 
+	subnet_id			= "${aws_subnet.sn-pub2.id}"
+	security_groups = ["${aws_security_group.sg-jumphost.id}"]
+
 	key_name = "${var.sshkey_name}"
 	associate_public_ip_address = true
 
-} 
+	tags {
+		Name = "jh2"
+	}
+
+}
 
 /* OUTPUT - IPs */
-output "NIC-jh1" {
+output "public_ip-jh1" {
 	value = "${aws_instance.vm-jh1.public_ip}"
 }
 
-output "NIC-jh2" {
+output "public_ip-jh2" {
 	value = "${aws_instance.vm-jh2.public_ip}"
 }
 
@@ -45,6 +84,7 @@ resource "aws_launch_configuration" "lc-web" {
   }
 }
 
+/*
 resource "aws_autoscaling_group" "asg-web" {
   name                 = "web-"
   launch_configuration = "${aws_launch_configuration.lc-web.name}"
@@ -55,4 +95,4 @@ resource "aws_autoscaling_group" "asg-web" {
   lifecycle {
     create_before_destroy = true
   }
-}
+} */
